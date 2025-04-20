55fbc776ab6dedb2b076f1b4dfc16a899f4fa353
Vladislav <kalachinskii_vl@mail.ru>
2025-04-20 03:10:54 +0300

    реализован лого + кнопка темы

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
+import { Logo } from "../Logo/Logo";
 import styles from "./App.module.css";
 
 function App() {
     return (
         <div className={styles.container}>
-            <h1 className={styles.test}>start</h1>
+            <Logo />
         </div>
     );
 }
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import styles from "./Logo.module.css";
+
+export function Logo() {
+    const [themeToggle, setThemeToggle] = useState<string>(() => {
+        return localStorage.getItem("theme") || "light";
+    });
+
+    // тема - старт страницы
+    useEffect(() => {
+        document.body.classList.toggle("darkTheme", themeToggle === "dark");
+    }, [themeToggle]);
+
+    // тема - кнопка
+    const toggleTheme = () => {
+        const newTheme = themeToggle === "light" ? "dark" : "light";
+        setThemeToggle(newTheme);
+        localStorage.setItem("theme", newTheme);
+    };
+
+    return (
+        <div className={styles.logoBox}>
+            <img
+                src={`${
+                    themeToggle == "light"
+                        ? "/image/logo/logo-light-theme.svg"
+                        : "/image/logo/logo-dark-theme.svg"
+                } `}
+                alt="Картинка логотипа"
+            />
+            <div onClick={toggleTheme} className={styles.themeBtn}>
+                <img
+                    src={`${
+                        themeToggle == "light"
+                            ? "/image/icon/icon-moon.svg"
+                            : "/image/icon/icon-sun.svg"
+                    } `}
+                    alt="картинка иконки смены темы"
+                />
+            </div>
+        </div>
+    );
+}
